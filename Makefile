b35dc38a768d98b1097831b5ca78ea52fbd900e9
embolson <embolson@pc8c0fkv7.inf.ufrgs.br>
2023-11-08 13:11:46 -0300

    update

--- Makefile
+++ Makefile
@@ -1,6 +1,5 @@
 CC = g++
 all: main
-	./main
 main: main.o
 	${CC} -o main main.o
 main.o: main.cpp
---
+++ client.cpp
@@ -0,0 +1,14 @@
+#include <string>
+
+class Client{
+    public:
+
+    Client(string name_in){
+        if (true){
+            
+        }
+    }
+
+    private:
+        string name;
+};
